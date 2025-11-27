3f652601b187208a3502786ad5c60bd23d2b93ed
Stu Schwartz <stu.schwartz@surrealdb.com>
2025-11-27 11:25:51 +0000

    Add support for Bytes. (#24)

--- src/impls.rs
+++ src/impls.rs
@@ -26,3 +26,32 @@ mod uuid {
 		}
 	}
 }
+
+#[cfg(feature = "bytes")]
+mod bytes {
+	use std::io::{BufRead, Write};
+
+	use ::bytes::Bytes;
+
+	use crate::{
+		BorrowDecode, BorrowReader, Decode, DecodeError, Encode, EncodeError, Reader, Writer,
+	};
+
+	impl<F> Encode<F> for Bytes {
+		fn encode<W: Write>(&self, w: &mut Writer<W>) -> Result<(), EncodeError> {
+			w.write_slice(self.as_ref())
+		}
+	}
+
+	impl<F> Decode<F> for Bytes {
+		fn decode<R: BufRead>(r: &mut Reader<R>) -> Result<Self, DecodeError> {
+			Ok(Bytes::from(r.read_vec()?))
+		}
+	}
+
+	impl<'de, F> BorrowDecode<'de, F> for Bytes {
+		fn borrow_decode(r: &mut BorrowReader<'de>) -> Result<Self, DecodeError> {
+			Ok(Bytes::from(r.read_vec()?))
+		}
+	}
+}
